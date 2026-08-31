0c15ec56c9cbcc6c29ac155583040a47dc565426
TemoshoS <temoshoshaku@gmail.com>
2026-08-31 17:30:28 +0200

    email

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,11 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_SECURE === "true",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+// Test SMTP connection
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP ERROR:", error);
+  } else {
+    console.log("✅ SMTP SERVER READY");
+  }
 });
 
 
